5f77fe73dfe0cee3fc2a6ab8db20b52aed27c54d
RodAbreu <abreu.rodrigo97@gmail.com>
2017-10-06 14:04:45 -0300

    First

---
+++ Trabalho1ORI.py
@@ -0,0 +1,35 @@
+
+def abertura(x):
+    print('Define sua escolha:\n')
+    print('[1] Criacao de um arquivo')
+    print('[2] Insercao de um novo registro')
+    print('[3] Buscar um registro')
+    print('[4] Remocao de um registro')
+    print('[5] Listagem dos registros')
+    print('[6] Compactacao do arquivo')
+    x = input('Digite sua escolha: ')
+    return x
+
+'''inicio do programa''' #bora lá peraltinha
+print('Bem-vindo ao trabalho 1!!\n\n')
+
+x = 1;
+while abertura(x)!=0:
+
+    if x==1:
+        pass
+    elif x==2:
+        pass
+    elif x==3:
+        pass
+    elif x==4:
+        pass
+    elif x==5:
+        pass
+    elif x==6:
+        pass
+    else:
+        x = 0
+        pass
+
+    print('\n\n')
