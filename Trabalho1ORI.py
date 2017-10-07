5b838fef80d995d081cc3e104d8f84ecf9725dac
RodAbreu <abreu.rodrigo97@gmail.com>
2017-10-07 15:13:31 -0300

    passos 2,3 e 5 feitos

--- Trabalho1ORI.py
+++ Trabalho1ORI.py
@@ -10,15 +10,58 @@ def abertura():
     print('[0] Sair')
     pass
 
-def check(nomeArquivo,busca):
-	arquivo = file(nomeArquivo+'.txt')
-	for line in arquivo:
+#opcao1
+def criaArquivo():
+    nomeArquivo = raw_input('Digite o nome do arquivo que voce deseja criar: ')
+    arquivo = open(nomeArquivo+'.txt','w')
+    arquivo.close()
+    pass
+
+#opcao2
+def addRegistro():
+    #acessando o arquivo
+    nomeArquivo = raw_input('Digite o nome do arquivo: ') #lendo o nome do arquivo
+    arquivo = open(nomeArquivo+'.txt','r') #abrindo para leitura
+    conteudo = arquivo.readlines() #lendo os dados do arquivo
+    nome = raw_input('Digite seu nome: ')
+    ra = raw_input('Digite seu ra: ')
+    idade = raw_input('Digite sua idade: ')
+    conteudo.append(nome+'||'+ra+'||'+idade+'||') #concatenando
+    arquivo.close()
+    arquivo = open(nomeArquivo+'.txt','w') #abrindo para escrita
+    if len(conteudo)<=99:
+        arquivo.writelines(conteudo) #adicionando conteudo
+    else:
+        print('Conteudo muito grande.')
+    #fechando
+    arquivo.close()
+    pass
+
+#opcao3
+def procuraRegistro():
+    nomeArquivo = raw_input('Digite o nome do arquivo: ')
+    busca = raw_input('Digite o que deseja buscar: ')
+    arquivo = file(nomeArquivo+'.txt')
+    for line in arquivo:
 		if busca in line:
 			found = True
 			break
 		else:
 			found = False
-	return found
+    return found
+
+#opcao4
+
+#opcao5
+def mostraRegistros():
+    nome = raw_input('Digite o nome do arquivo que voce deseja acessar: ')
+    print open(nome+'.txt').read()
+    pass
+
+#opcao6
+
+
+
 
 
 print('Bem-vindo ao trabalho 1!!\n\n')
@@ -30,41 +73,29 @@ while x!=0:
     x = input('Digite sua escolha: ')
 
     if x==1:
-        nomeArquivo = raw_input('Digite o nome do arquivo que voce deseja criar: ')
-        arquivo = open(nomeArquivo+'.txt','w')
-        arquivo.close()
+        criaArquivo()
         pass
 
     elif x==2:
-        #acessando o arquivo
-        nomeArquivo = raw_input('Digite o nome do arquivo: ') #lendo o nome do arquivo
-        arquivo = open(nomeArquivo+'.txt','r') #abrindo para leitura
-        conteudo = arquivo.readlines() #lendo os dados do arquivo
-        nome = raw_input('Digite seu nome: ')
-        ra = raw_input('Digite seu ra: ')
-        conteudo.append(nome+'||'+ra+'||') #concatenando
-        arquivo.close()
-        arquivo = open(nomeArquivo+'.txt','w') #abrindo para escrita
-        arquivo.writelines(conteudo) #adicionando conteudo
-        #fechando
-        arquivo.close()
+        addRegistro()
         pass
 
     elif x==3:
         #bucando um registro
-        nomeArquivo = raw_input('Digite o nome do arquivo: ')
-        busca = raw_input('Digite o que deseja buscar: ')
-        encontrou = check(nomeArquivo,busca)
+        encontrou = procuraRegistro()
         print(encontrou)
         pass
+
     elif x==4:
         pass
+
     elif x==5:
-        nome = raw_input('Digite o nome do arquivo que voce deseja acessar: ')
-        print open(nome+'.txt').read()
+        mostraRegistros()
         pass
+
     elif x==6:
         pass
+    
     else:
         x = 0
         pass
