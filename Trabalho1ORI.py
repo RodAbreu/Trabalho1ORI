18db525f114c4bc81b5910cadee2ebb6e061b455
RodAbreu <abreu.rodrigo97@gmail.com>
2017-10-07 00:18:55 -0300

    Ainda não consigo adicionar mais de um

--- Trabalho1ORI.py
+++ Trabalho1ORI.py
@@ -1,5 +1,5 @@
 
-def abertura(x):
+def abertura():
     print('Define sua escolha:\n')
     print('[1] Criacao de um arquivo')
     print('[2] Insercao de um novo registro')
@@ -7,24 +7,38 @@ def abertura(x):
     print('[4] Remocao de um registro')
     print('[5] Listagem dos registros')
     print('[6] Compactacao do arquivo')
-    x = input('Digite sua escolha: ')
-    return x
+    print('[0] Sair')
+    pass
 
-'''inicio do programa''' #bora lá peraltinha
 print('Bem-vindo ao trabalho 1!!\n\n')
-
-x = 1;
-while abertura(x)!=0:
+import sys
+import os.path
+x=1
+while x!=0:
+    abertura()
+    x = input('Digite sua escolha: ')
 
     if x==1:
+        nomeArquivo = raw_input('Digite o nome do arquivo que voce deseja criar: ')
+        arquivo = open(nomeArquivo+'.txt','w')
+        arquivo.close()
         pass
     elif x==2:
-        pass
+        nomeArquivo = raw_input('Digite o nome do arquivo que voce deseja acessar: ')
+        arquivo = open(nomeArquivo+'.txt','w')
+        nome = raw_input('Digite seu nome: ')
+        arquivo.write(nome+'||')
+        ra = raw_input('Digite seu ra: ')
+        arquivo.write(ra+'||')
+        arquivo.close()
+
     elif x==3:
         pass
     elif x==4:
         pass
     elif x==5:
+        nome = raw_input('Digite o nome do arquivo que voce deseja acessar: ')
+        print open(nome+'.txt').read()
         pass
     elif x==6:
         pass
@@ -33,3 +47,5 @@ while abertura(x)!=0:
         pass
 
     print('\n\n')
+
+print('Fim do programa.')
